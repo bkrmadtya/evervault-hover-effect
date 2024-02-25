1582cf401247d822407789cc534903ff2b931d24
Bikram Karki <39879795+bkrmadtya@users.noreply.github.com>
2024-02-26 00:53:41 +0200

    feat: added logic for tourch effect

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Connected!");
+// Helper functions
+
+const getElementById = (id) => {
+  return document.getElementById(id);
+};
+
+// Main code
+
+const container = getElementById("container");
+const mask = getElementById("mask");
+const bgText = getElementById("bg-text");
+
+const maskRadius = 200;
+
+container.addEventListener("mousemove", ({ target, clientX, clientY }) => {
+  const { left, top } = target.getBoundingClientRect();
+  const x = clientX - left;
+  const y = clientY - top;
+
+  mask.style.opacity = 1;
+  mask.style.maskImage = `radial-gradient(circle ${maskRadius}px at ${x}px ${y}px, black, transparent 100%)`;
+});
+
+container.addEventListener("mouseleave", () => {
+  bgText.innerText = "";
+  mask.style.opacity = 0;
+});
